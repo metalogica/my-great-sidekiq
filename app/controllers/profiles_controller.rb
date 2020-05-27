b90a0a362625a0d2fdd27913616eaee521ba699f
richard jarram <rjarram@me.com>
2020-05-26 22:22:58 -0400

    init

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,19 @@
+class ProfilesController < ApplicationController
+    def update 
+      if current_user.update(user_params)
+        UpdateUserJob.perform_later(current_user.id) # <- The enrichment API ob is queued
+        flash[:notice] = "Your profile has been updated"
+        redirect_to root_path
+      else
+        flash[:notice] = "There was an error"
+        redirect_to root_path
+      end
+    end
+  
+    private
+  
+    def user_params
+      # Some strong params of your choice
+      params.require(:user).permit(:email)
+    end
+  end
